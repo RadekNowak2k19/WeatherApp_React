2bdc3fa9a59f53c9561693debea7da5088c370a9
Radosław Nowak <radeknowak2k19@gmail.com>
2023-09-14 16:05:17 +0200

    FooterComponent

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,11 @@
 import styles from "./Footer.module.scss";
 export const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div className={styles.footer}>
+			<p>
+				Created using the <strong>OpenWeatherMap</strong> &copy; All rights
+				reserved.
+			</p>
+		</div>
+	);
 };
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,4 +1,4 @@
 import styles from "./MainContainer.module.scss";
 export const MainContainer = () => {
-	return <div>MainContainer</div>;
+	return <main className={styles.main}>MainContainer</main>;
 };
