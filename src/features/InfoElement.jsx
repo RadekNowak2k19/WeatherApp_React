d66ae5fcc56b954e0c46d6e5775962f41cef98f3
Radosław Nowak <radeknowak2k19@gmail.com>
2023-09-15 16:43:57 +0200

    Created Info component

--- src/features/InfoElement.jsx
+++ src/features/InfoElement.jsx
@@ -1,9 +1,17 @@
 import styles from "./InfoElement.module.scss";
-export const InfoElement = ({ temp, title }) => {
-	return (
-		<div className={styles.infoElement}>
-			<span>{title}: </span>
-			<span>{temp}</span>
-		</div>
-	);
+export const InfoElement = ({ temp = null, title, info = null }) => {
+	if (temp)
+		return (
+			<div className={styles.infoElement}>
+				<span>{title}: </span>
+				<span>{temp}</span>
+			</div>
+		);
+	if (temp === null)
+		return (
+			<div className={styles.infoElement}>
+				<span>{title}: </span>
+				<span>{info}</span>
+			</div>
+		);
 };
