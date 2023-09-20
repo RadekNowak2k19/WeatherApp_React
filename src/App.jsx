5e36e0205a12b170f2b03fa0f5265a7bc23742da
Radosław Nowak <radeknowak2k19@gmail.com>
2023-09-20 13:34:25 +0200

    Added the ability to search for any city.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-// Hooks
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 // Components
 import { Header } from "./components/Header";
 import { MainContainer } from "./components/MainContainer";
@@ -10,11 +9,12 @@ import { Error } from "./components/Error";
 // OpenWeatherMap
 const API_KEY = "f19cccf1d9b5d6373110845d2578547c";
 const UNITS = "metric";
-const URL = `https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=${API_KEY}&units=${UNITS}`;
-const FORECAST = `https://api.openweathermap.org/data/2.5/forecast?q=Warsaw&appid=f19cccf1d9b5d6373110845d2578547c&units=${UNITS}`;
+// const URL = `https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=${API_KEY}&units=${UNITS}`;
+// const FORECAST = `https://api.openweathermap.org/data/2.5/forecast?q=Warsaw&appid=f19cccf1d9b5d6373110845d2578547c&units=${UNITS}`;
 
 const initialState = {
 	currentWeather: {},
+	currentCity: "Warsaw",
 	// loading, error, ready
 	status: "loading",
 	forecast: [],
@@ -27,49 +27,60 @@ function reducer(state, action) {
 			return { ...state, forecast: action.payload, status: "ready" };
 		case "dataFailed":
 			return { ...state, status: "error" };
+		case "searchCity":
+			return { ...state, currentCity: action.payload, status: "ready" };
 		default:
 			throw new Error("Unknown error :(");
 	}
 }
 function App() {
-	// const [currentWeather, setCurrentWeather] = useState({});
+	const [city, setCity] = useState("");
 	const [state, dispatch] = useReducer(reducer, initialState);
-	const { currentWeather, status, forecast } = state;
+	const { currentWeather, status, forecast, currentCity } = state;
 
 	// useEffect currentWeather
-	useEffect(function () {
-		async function weatherFetch() {
-			try {
-				const res = await fetch(URL);
-				const data = await res.json();
-				dispatch({ type: "dataRecived", payload: data });
-			} catch (err) {
-				dispatch({ type: "dataFailed" });
+	useEffect(
+		function () {
+			async function weatherFetch() {
+				try {
+					const res = await fetch(
+						`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${API_KEY}&units=${UNITS}`
+					);
+					const data = await res.json();
+					dispatch({ type: "dataRecived", payload: data });
+				} catch (err) {
+					dispatch({ type: "dataFailed" });
+				}
 			}
-		}
-		weatherFetch();
-	}, []);
+			weatherFetch();
+		},
+		[currentCity]
+	);
 	// useEffect forecastWeather
-	useEffect(function () {
-		async function weatherFetch() {
-			try {
-				const res = await fetch(FORECAST);
-				const data = await res.json();
-				dispatch({ type: "dateForecastRecived", payload: data });
-			} catch (err) {
-				dispatch({ type: "dataFailed" });
+	useEffect(
+		function () {
+			async function weatherFetch() {
+				try {
+					const res = await fetch(
+						`https://api.openweathermap.org/data/2.5/forecast?q=${currentCity}&appid=f19cccf1d9b5d6373110845d2578547c&units=${UNITS}`
+					);
+					const data = await res.json();
+					dispatch({ type: "dateForecastRecived", payload: data });
+				} catch (err) {
+					dispatch({ type: "dataFailed" });
+				}
 			}
-		}
-		weatherFetch();
-	}, []);
+			weatherFetch();
+		},
+		[currentCity]
+	);
 
 	// cod, name, id, wind: {speed, deg}, timezone, main: {temp, temp_max, temp_min, pressure, feels_like}, sys: {country, sunrise, sunset}, weather: [{description, mian, id , icon}]
 	const { name, sys, main, weather, wind } = currentWeather;
 	const forecastArr = forecast.list;
-	console.log(currentWeather);
 	return (
 		<>
-			<Header />
+			<Header dispatch={dispatch} city={city} setCity={setCity} />
 			<MainContainer>
 				{status === "loading" && <Loading />}
 				{status === "error" && <Error />}
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,19 @@
 import { searchIcon } from "../assets/images/index";
-export const Search = () => {
+export const Search = ({ dispatch, city, setCity }) => {
+	const handleSearchCity = () => {
+		if (!city) return;
+		dispatch({ type: "searchCity", payload: city });
+	};
+	// dispatch({ type: "searchCity", payload: e.target.value }
 	return (
-		<form className="search">
-			<input className="input" type="text" />
-			<button>
+		<form className="search" onSubmit={e => e.preventDefault()}>
+			<input
+				className="input"
+				type="text"
+				value={city}
+				onChange={e => setCity(e.target.value)}
+			/>
+			<button onClick={handleSearchCity}>
 				<img src={searchIcon} alt="Search icon inner button element" />
 			</button>
 		</form>
